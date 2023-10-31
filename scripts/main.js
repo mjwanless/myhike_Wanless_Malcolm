bf7c1afe3ca51cc7795c2b41efa68b01e0701ba1
Malcolm Wanless <mwanless2@my.bcit.ca>
2023-10-31 11:41:16 -0700

    Quick checkpoint for docInfo

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,35 @@
 function getNameFromAuth() {
-   firebase.auth().onAuthStateChanged((user) => {
-      // Check if a user is signed in:
-      if (user) {
-         // Do something for the currently logged-in user here:
-         console.log(user.uid); //print the uid in the browser console
-         console.log(user.displayName); //print the user name in the browser console
-         userName = user.displayName;
+    firebase.auth().onAuthStateChanged((user) => {
+        // Check if a user is signed in:
+        if (user) {
+            // Do something for the currently logged-in user here:
+            console.log(user.uid); //print the uid in the browser console
+            console.log(user.displayName); //print the user name in the browser console
+            userName = user.displayName;
 
-         //method #1:  insert with JS
-         document.getElementById("name-goes-here").innerText = userName;
+            //method #1:  insert with JS
+            document.getElementById("name-goes-here").innerText = userName;
 
-         //method #2:  insert using jquery
-         //$("#name-goes-here").text(userName); //using jquery
+            //method #2:  insert using jquery
+            //$("#name-goes-here").text(userName); //using jquery
 
-         //method #3:  insert using querySelector
-         //document.querySelector("#name-goes-here").innerText = userName
-      } else {
-         // No user is signed in.
-      }
-   });
+            //method #3:  insert using querySelector
+            //document.querySelector("#name-goes-here").innerText = userName
+        } else {
+            // No user is signed in.
+        }
+    });
 }
 getNameFromAuth(); //run the function
+
+function readQuote(day) {
+    db.collection("quotes")
+        .doc(day)
+        .onSnapshot((dayInfo) => {
+            console.log(dayInfo.data());
+            quoteOfTheDay = dayInfo.data().quote;
+            document.getElementById("quote-goes-here").innerHTML = quoteOfTheDay;
+        });
+}
+
+readQuote("tuesday");
